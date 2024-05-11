ecd8c8382842fd036f945c96adc4f64088a07f9e
juanconde025 <juanconde1309@gmail.com>
2024-05-11 13:12:51 -0500

    fix: cambios en la funcion de ventas

--- ventas.py
+++ ventas.py
@@ -5,7 +5,7 @@ from Modulos.Servicios.datos import *
 
 RUTA = "Modulos/Administrativo/CRUD.json"
 RUTA_SERVICIOS = "Servicios/serviciosyproductos.json"
-RUTA_VENTAS = "Ventas/ventas.json"
+RUTA_VENTAS = "Modulos/Ventas/ventas.json"
 
 datos = cargar_datos_ventas(RUTA)
 #datos_servicios = cargar_datos_servicios(RUTA_SERVICIOS)
@@ -14,5 +14,10 @@ def ventas_totales(datos:dict):
     for i in datos["usuarios"]:
         if "servicios" in i:
             print(i["servicios"])
+    datos = i["servicios"]
+    return datos
+
+datos = guardar_datos_servicios(datos, RUTA_VENTAS)
+
 
  
