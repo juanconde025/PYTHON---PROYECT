fcd69bafef3b73217ed879cb9d9241a344b7cbac
juanconde025 <juanconde1309@gmail.com>
2024-05-13 09:47:34 -0500

    fix: cambios

--- Menu/menu.py
+++ Menu/menu.py
@@ -1,47 +1,47 @@
 def menu_principal():
-    print("*************")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
     print("Bienvenido a nuestro menu interactivo de Claro\n en él te presentamos las siguientes opciones")
-    print("*************")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
     print("Ingrese la opción que requiera")
     print("1. para ir al modulo administrativo de usuarios")
     print("2. para ir al modulo administrativo de servicios")
     print("3. para ir al modulo de reportes")
     print("4. para ir al modulo de ventas")
     print("5. para salir")
-    print("*************")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 
 def menu_usuarios():
-    print("*************")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
     print("Ingrese la opción que requiera")
     print("1. para crear un usuario")
     print("2. para ver la informacion de un usuario")
     print("3. para actualizar a un usuario")
     print("4. para eliminar un usuario")
     print("5. para ver la categoria del usuario")
-    print("*************")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 
 def menu_servicios():
-    print("*************")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
     print("Ingrese la opción que requiera")
     print("1. para crear un servicio")
     print("2. para ver el catalogo de servicios")
     print("3. para actualizar un servicio")
     print("4. para eliminar un servicio")
-    print("*************")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 
 def menu_reportes():
-    print("*************")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
     print("Ingrese la opción que requiera")
     print("1. para ver la cantidad de productos y servicios que hay en la empresa")
-    print("*************")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 
 def menu_ventas():
-    print("*************")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
     print("Ingrese la opción que requiera")
     print("1. para ver el registro de ventas totales")
     print("2. para comprar un producto")
     print("3. para comprar un servicio")
-    print("*************")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
     
 def pedir_opcion():
     opc = 0
